e8a3a308e271b5d26eeb4c0fe783ee342638fcbc
Maksim Ivanov <satansdeer@gmail.com>
2019-09-01 16:32:56 +0200

    Load list of images

--- src/pages/portrait.js
+++ src/pages/portrait.js
@@ -8,421 +8,50 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Img from "gatsby-image"
 
-const PortraitPage = ({data}) => (
-  <Layout>
-    <SEO title="Роман Корнилов — Портрет" />
-    {/* <MasonryPage /> */}
-    <div>
-      <Img fluid={data.file.childImageSharp.fluid} />
-    </div>
-    <div id="gallery-lightbox" className="spacing-top-gallery">
-      <div className="row pt-2 mt-4">
-        <div className="col-md-12">
-          <div className="mdb-lightbox">
-            <figure className="col-md-4">
-              <a
-                href="../images/portrait/big/min/portrait_106.jpg"
-                data-size="1920x1280"
-              >
-                <img
-                  alt="portrait"
-                  src="../images/portrait/small/min/portrait_106.jpg"
-                  className="img-fluid"
-                />
-              </a>
-            </figure>
-
-            <figure className="col-md-4">
-              <a
-                href="../images/portrait/big/min/portrait_107.jpg"
-                data-size="1920x1280"
-              >
-                <img
-                  alt="portrait"
-                  src="../images/portrait/small/min/portrait_107.jpg"
-                  className="img-fluid"
-                />
-              </a>
-            </figure>
-
-            <figure className="col-md-4">
-              <a
-                href="../images/portrait/big/min/portrait_108.jpg"
-                data-size="1920x1280"
-              >
-                <img
-                  alt="portrait"
-                  src="../images/portrait/small/min/portrait_108.jpg"
-                  className="img-fluid"
-                />
-              </a>
-            </figure>
-
-            <figure className="col-md-4">
-              <a
-                href="../images/portrait/big/min/portrait_109.jpg"
-                data-size="1920x1280"
-              >
-                <img
-                  alt="portrait"
-                  src="../images/portrait/small/min/portrait_109.jpg"
-                  className="img-fluid"
-                />
-              </a>
-            </figure>
-
-            <figure className="col-md-4">
-              <a
-                href="../images/portrait/big/min/portrait_60.jpg"
-                data-size="1920x1280"
-              >
-                <img
-                  alt="portrait"
-                  src="../images/portrait/small/min/portrait_60.jpg"
-                  className="img-fluid"
-                />
-              </a>
-            </figure>
-
-            <figure className="col-md-4">
-              <a
-                href="../images/portrait/big/min/portrait_112.jpg"
-                data-size="1920x1280"
-              >
-                <img
-                  alt="portrait"
-                  src="../images/portrait/small/min/portrait_112.jpg"
-                  className="img-fluid"
-                />
-              </a>
-            </figure>
-
-            <figure className="col-md-4">
-              <a
-                href="../images/portrait/big/min/portrait_56.jpg"
-                data-size="1920x1280"
-              >
-                <img
-                  alt="portrait"
-                  src="../images/portrait/small/min/portrait_56.jpg"
-                  className="img-fluid"
-                />
-              </a>
-            </figure>
-
-            <figure className="col-md-4">
-              <a
-                href="../images/portrait/big/min/portrait_100.jpg"
-                data-size="1920x1280"
-              >
-                <img
-                  alt="portrait"
-                  src="../images/portrait/small/min/portrait_100.jpg"
-                  className="img-fluid"
-                />
-              </a>
-            </figure>
-
-            <figure className="col-md-4">
-              <a
-                href="../images/portrait/big/min/portrait_53.jpg"
-                data-size="1920x1280"
-              >
-                <img
-                  alt="portrait"
-                  src="../images/portrait/small/min/portrait_53.jpg"
-                  className="img-fluid"
-                />
-              </a>
-            </figure>
-
-            <figure className="col-md-4">
-              <a
-                href="../images/portrait/big/min/portrait_102.jpg"
-                data-size="1920x1280"
-              >
-                <img
-                  alt="portrait"
-                  src="../images/portrait/small/min/portrait_102.jpg"
-                  className="img-fluid"
-                />
-              </a>
-            </figure>
-
-            <figure className="col-md-4">
-              <a
-                href="../images/portrait/big/min/portrait_105.jpg"
-                data-size="1920x1280"
-              >
-                <img
-                  alt="portrait"
-                  src="../images/portrait/small/min/portrait_105.jpg"
-                  className="img-fluid"
-                />
-              </a>
-            </figure>
-
-            <figure className="col-md-4">
-              <a
-                href="../images/portrait/big/min/portrait_52.jpg"
-                data-size="1920x1280"
-              >
-                <img
-                  alt="portrait"
-                  src="../images/portrait/small/min/portrait_52.jpg"
-                  className="img-fluid"
-                />
-              </a>
-            </figure>
-
-            <figure className="col-md-4">
-              <a
-                href="../images/portrait/big/min/portrait_72.jpg"
-                data-size="1920x1280"
-              >
-                <img
-                  alt="portrait"
-                  src="../images/portrait/small/min/portrait_72.jpg"
-                  className="img-fluid"
-                />
-              </a>
-            </figure>
-
-            <figure className="col-md-4">
-              <a
-                href="../images/portrait/big/min/portrait_79.jpg"
-                data-size="1920x1280"
-              >
-                <img
-                  alt="portrait"
-                  src="../images/portrait/small/min/portrait_79.jpg"
-                  className="img-fluid"
-                />
-              </a>
-            </figure>
-
-            <figure className="col-md-4">
-              <a
-                href="../images/portrait/big/min/portrait_78.jpg"
-                data-size="1920x1280"
-              >
-                <img
-                  alt="portrait"
-                  src="../images/portrait/small/min/portrait_78.jpg"
-                  className="img-fluid"
-                />
-              </a>
-            </figure>
-
-            <figure className="col-md-4">
-              <a
-                href="../images/portrait/big/min/portrait_103.jpg"
-                data-size="1920x1280"
-              >
-                <img
-                  alt="portrait"
-                  src="../images/portrait/small/min/portrait_103.jpg"
-                  className="img-fluid"
-                />
-              </a>
-            </figure>
-
-            <figure className="col-md-4">
-              <a
-                href="../images/portrait/big/min/portrait_14.jpg"
-                data-size="1920x1280"
-              >
-                <img
-                  alt="portrait"
-                  src="../images/portrait/small/min/portrait_14.jpg"
-                  className="img-fluid"
-                />
-              </a>
-            </figure>
-
-            <figure className="col-md-4">
-              <a
-                href="../images/portrait/big/min/portrait_61.jpg"
-                data-size="1920x1280"
-              >
-                <img
-                  alt="portrait"
-                  src="../images/portrait/small/min/portrait_61.jpg"
-                  className="img-fluid"
-                />
-              </a>
-            </figure>
-
-            <figure className="col-md-4">
-              <a
-                href="../images/portrait/big/min/portrait_8.jpg"
-                data-size="853x1280"
-              >
-                <img
-                  alt="portrait"
-                  src="../images/portrait/small/min/portrait_8.jpg"
-                  className="img-fluid"
-                />
-              </a>
-            </figure>
-
-            <figure className="col-md-4">
-              <a
-                href="../images/portrait/big/min/portrait_60-n.jpg"
-                data-size="853x1280"
-              >
-                <img
-                  alt="portrait"
-                  src="../images/portrait/small/min/portrait_60-n.jpg"
-                  className="img-fluid"
-                />
-              </a>
-            </figure>
-
-            <figure className="col-md-4">
-              <a
-                href="../images/portrait/big/min/portrait_63.jpg"
-                data-size="853x1280"
-              >
-                <img
-                  alt="portrait"
-                  src="../images/portrait/small/min/portrait_63.jpg"
-                  className="img-fluid"
-                />
-              </a>
-            </figure>
-
-            <figure className="col-md-4">
-              <a
-                href="../images/portrait/big/min/portrait_17.jpg"
-                data-size="853x1280"
-              >
-                <img
-                  alt="portrait"
-                  src="../images/portrait/small/min/portrait_17.jpg"
-                  className="img-fluid"
-                />
-              </a>
-            </figure>
-
-            <figure className="col-md-4">
-              <a
-                href="../images/portrait/big/min/portrait_16.jpg"
-                data-size="853x1280"
-              >
-                <img
-                  alt="portrait"
-                  src="../images/portrait/small/min/portrait_16.jpg"
-                  className="img-fluid"
-                />
-              </a>
-            </figure>
-
-            <figure className="col-md-4">
-              <a
-                href="../images/portrait/big/min/portrait_110.jpg"
-                data-size="853x1280"
-              >
-                <img
-                  alt="portrait"
-                  src="../images/portrait/small/min/portrait_110.jpg"
-                  className="img-fluid"
-                />
-              </a>
-            </figure>
-
-            <figure className="col-md-4">
-              <a
-                href="../images/portrait/big/min/portrait_22.jpg"
-                data-size="853x1280"
-              >
-                <img
-                  alt="portrait"
-                  src="../images/portrait/small/min/portrait_22.jpg"
-                  className="img-fluid"
-                />
-              </a>
-            </figure>
-
-            <figure className="col-md-4">
-              <a
-                href="../images/portrait/big/min/portrait_26.jpg"
-                data-size="853x1280"
-              >
-                <img
-                  alt="portrait"
-                  src="../images/portrait/small/min/portrait_26.jpg"
-                  className="img-fluid"
-                />
-              </a>
-            </figure>
-
-            <figure className="col-md-4">
-              <a
-                href="../images/portrait/big/min/portrait_27.jpg"
-                data-size="853x1280"
-              >
-                <img
-                  alt="portrait"
-                  src="../images/portrait/small/min/portrait_27.jpg"
-                  className="img-fluid"
-                />
-              </a>
-            </figure>
-
-            <figure className="col-md-4">
-              <a
-                href="../images/portrait/big/min/portrait_111.jpg"
-                data-size="853x1280"
-              >
-                <img
-                  alt="portrait"
-                  src="../images/portrait/small/min/portrait_111.jpg"
-                  className="img-fluid"
-                />
-              </a>
-            </figure>
-
-            <figure className="col-md-4">
-              <a
-                href="../images/portrait/big/min/portrait_101.jpg"
-                data-size="853x1280"
-              >
-                <img
-                  alt="portrait"
-                  src="../images/portrait/small/min/portrait_101.jpg"
-                  className="img-fluid"
-                />
-              </a>
-            </figure>
-
-            <figure className="col-md-4">
-              <a
-                href="../images/portrait/big/min/portrait_104.jpg"
-                data-size="853x1280"
-              >
-                <img
-                  alt="portrait"
-                  src="../images/portrait/small/min/portrait_104.jpg"
-                  className="img-fluid"
-                />
-              </a>
-            </figure>
+const PortraitPage = ({ data }) => {
+  console.log(data)
+  return (
+    <Layout>
+      <SEO title="Роман Корнилов — Портрет" />
+      {/* <MasonryPage /> */}
+      <div id="gallery-lightbox" className="spacing-top-gallery">
+        <div className="row pt-2 mt-4">
+          <div className="col-md-12">
+            <div className="mdb-lightbox">
+              {data.allFile.edges.map(({node}) => (
+                <figure className="col-md-4">
+                  <a
+                    href="../images/portrait/big/min/portrait_106.jpg"
+                    data-size="1920x1280"
+                  >
+                    <Img alt="portrait" className="img-fluid" fluid={node.childImageSharp.fluid} />
+                  </a>
+                </figure>
+              ))}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </Layout>
-)
-
+    </Layout>
+  )
+}
 export default PortraitPage
 
 export const query = graphql`
-  query {
-    file(relativePath: { eq: "slider/slider_1.jpg" }) {
-      childImageSharp {
-        fluid {
-          ...GatsbyImageSharpFluid
+  {
+    allFile(
+      filter: {
+        extension: { regex: "/(jpg)|(png)/" }
+        relativeDirectory: { eq: "slider" }
+      }
+    ) {
+      edges {
+        node {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
         }
       }
     }
